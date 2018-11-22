efd640e163a393516e251b2d05f4e21c206ad53b
SVSergej <ser4102908@yandex.ru>
2018-11-23 00:46:50 +0300

    up

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! Новая ветка");
+        System.out.println("Hello World!");
+        System.out.println("branch2");
     }
 }
